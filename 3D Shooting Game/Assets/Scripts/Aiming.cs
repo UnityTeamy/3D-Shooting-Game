61dc245f11f42376cd37be7aeba974cb1238b1b0
unitylike <109219136+unitylike@users.noreply.github.com>
2022-12-25 23:49:55 +0900

    aiming, places, granade, bullet, game over
    aiming_camera speed
    places_duplicated
    grenade_1 shot 1 kill
    bullet_ 3 shot 1 kill
    game over_ restart/exit button

--- 3D Shooting Game/Assets/Scripts/Aiming.cs
+++ 3D Shooting Game/Assets/Scripts/Aiming.cs
@@ -11,11 +11,14 @@ public class Aiming : MonoBehaviour
     public float NormalFov = 60.0f;
     public float AimingFov = 10.0f;
     public float speed;
+    //public bool isAiming;
+    public Player player_code;
 
     // Start is called before the first frame update
     void Start()
     {
         FieldOfView = 60.0f;
+        //isAiming = false;
     }
 
     // Update is called once per frame
@@ -28,6 +31,8 @@ public class Aiming : MonoBehaviour
         if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
             //Aiming
+            //isAiming = true;
+            player_code.lookSensitivity = 1.5f;
             Frame.SetActive(true);
             if (FieldOfView <= AimingFov)
             {
@@ -41,6 +46,8 @@ public class Aiming : MonoBehaviour
         else
         {
             //not Aiming
+            //isAiming = false;
+            player_code.lookSensitivity = 3;
             Frame.SetActive(false);
             if (FieldOfView >= NormalFov)
             {
